b48b2b9c9bc9f974848216a6ea2bebd2b96cdd76
Ale <charlie14_1939@hotmail.com>
2023-12-22 18:55:33 -0300

    Add 9x5 2d grid

--- Scenes/board.gd
+++ Scenes/board.gd
@@ -3,6 +3,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for j in range(5):			
+		for i in range(9):
+			var cellScene = preload("res://Scenes/cell.tscn")
+			var cell_instance = cellScene.instantiate()
+			var cloneNode = cell_instance as Node2D
+			var new_node_x_position = 95*i
+			var new_node_y_position = 95*j
+			cloneNode.position.x = new_node_x_position
+			cloneNode.position.y = new_node_y_position
+			add_child(cloneNode)
+	print('ready')
+		
+	print(self.get_child_count());
 	pass # Replace with function body.
 
 
